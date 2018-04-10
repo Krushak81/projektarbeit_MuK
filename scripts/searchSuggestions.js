a2ef310f4da8023a2f9f549cf8d8a8c0d721aeba
Krushak81 <balintbenedikt@googlemail.com>
2018-04-10 14:08:51 +0200

    detail-location usw.

---
+++ scripts/searchSuggestions.js
@@ -0,0 +1,49 @@
+/*
+
+function searchSuggestions() {
+
+    var searchBar = document.getElementById("SearchBarLocations");
+
+    var availableTags = [
+        "ActionScript",
+        "Offenburg",
+        "AppleScript",
+        "Asp",
+        "BASIC",
+        "C",
+        "C++",
+        "Clojure",
+        "COBOL",
+        "ColdFusion",
+        "Cycle",
+        "Erlang",
+        "Fortran",
+        "Groovy",
+        "Haskell",
+        "Java",
+        "JavaScript",
+        "Lisp",
+        "Perl",
+        "PHP",
+        "Python",
+        "Ruby",
+        "Scala",
+        "Scheme"];
+
+    searchBar.focus(function(){
+        searchBar.autocomplete({
+            source: availableTags
+        });
+
+        searchBar.change(function () {
+            alert(searchBar.val());
+        });
+    });
+
+
+}*/
+
+
+
+
+
